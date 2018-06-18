f2153a9f4a4a4757ea2943e8d1f5a80eb0b3b759
jxv <joevargas92@gmail.com>
2018-06-17 21:12:57 -0700

    init

---
+++ MyIP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyIP
+//
+//  Created by Joe Vargas on 6/17/18.
+//  Copyright © 2018 Joe Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
