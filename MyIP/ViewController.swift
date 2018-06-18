dbc62bb8659de9b0359676662e3c04c2989acdb9
jxv <joevargas92@gmail.com>
2018-06-18 10:00:44 -0700

    request httpbin for ip

--- MyIP/ViewController.swift
+++ MyIP/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ipAddressLabel: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getMyIP(completion: updateIp);
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func updateIp(result: Result<IpResponse>) -> Void {
+        switch result {
+        case .success(let resp):
+            ipAddressLabel!.text = resp.origin;
+        case .failure:
+            return ();
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +35,49 @@ class ViewController: UIViewController {
 
 }
 
+
+enum Result<Value> {
+    case success(Value)
+    case failure(Error)
+}
+
+struct IpResponse: Codable {
+    let origin: String
+}
+
+func getMyIP(completion: ((Result<IpResponse>) -> Void)?) {
+    var request = URLRequest(url: URL(string: "https://httpbin.org/ip")!)
+    request.httpMethod = "GET"
+    
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config)
+    let task = session.dataTask(with: request) { (responseData, response, responseError) in
+        DispatchQueue.main.async {
+            if let error = responseError {
+                completion?(.failure(error))
+            } else if let jsonData = responseData {
+                // Now we have jsonData, Data representation of the JSON returned to us
+                // from our URLRequest...
+                
+                // Create an instance of JSONDecoder to decode the JSON data to our
+                // Codable struct
+                let decoder = JSONDecoder()
+                
+                do {
+                    // We would use Post.self for JSON representing a single Post
+                    // object, and [Post].self for JSON representing an array of
+                    // Post objects
+                    let ipResp = try decoder.decode(IpResponse.self, from: jsonData)
+                    completion?(.success(ipResp))
+                } catch {
+                    completion?(.failure(error))
+                }
+            } else {
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Data was not retrieved from request"]) as Error
+                completion?(.failure(error))
+            }
+        }
+    }
+    
+    task.resume()
+}
